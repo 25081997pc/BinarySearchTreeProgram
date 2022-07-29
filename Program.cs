f82d4373e9192ba1c53e007baa46affc913e4c73
25081997pc <chavanpooja2508@gmail.com>
2022-07-29 20:20:54 +0530

    Creating BST

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to The Binary Search Program");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
+            Console.ReadKey();
         }
     }
 }
