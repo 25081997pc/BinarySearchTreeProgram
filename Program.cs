9209315b68736a3feca8b41ab087b93736c07cff
25081997pc <chavanpooja2508@gmail.com>
2022-07-29 20:17:08 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BinarySearchTree_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Binary Search Program");
+        }
+    }
+}
+
